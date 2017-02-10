bddefba9d920e68a5bdeef18324687042b057170
Roman <roman0tershak@gmail.com>
2017-02-11 01:01:33 +0200

    delete unused lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'categories/new'
-
-  get 'categories/create'
-
-  get 'categories/update'
-
-  get 'categories/edit'
-
-  get 'categories/destroy'
-
-  get 'categories/index'
-
-  get 'categories/show'
-
   root 'static_pages#home'
   resources :categories
 end
