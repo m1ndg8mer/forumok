587300572fd1cedc0fd7de3449135f6f6149761e
Roman <roman0tershak@gmail.com>
2017-03-04 00:05:28 +0200

    Add design for landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    @clients_count = User.all.count
+    @categories_count = Category.published.count
+    @messages_count = Message.count
+    @roles_count = Role.all.count
+    @last_categories = Category.published.limit(3)
   end
 end
